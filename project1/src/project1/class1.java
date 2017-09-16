e3bc604d1b95fb76c87bdcf7a1d4b12a93292e73
RAAGA\sahitya <jubin.yadllapalli@gmail.com>
2017-09-16 16:15:12 -0500

    class1 commited

---
+++ project1/src/project1/class1.java
@@ -0,0 +1,16 @@
+package project1;
+import java.util.Scanner;
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner ui = new Scanner(System.in);
+		class2 obj = new class2();
+		System.out.println("enter usrname :");
+		String username = ui.nextLine();
+		obj.text(username);
+
+	}
+
+}
